548849dc26981be53f60fad95ee49057f82bb1de
paiva <lcspaiva87@gmail.com>
2024-05-18 20:54:36 -0300

    feat: add Highlight component for displaying titles and subtitles

--- src/screen/Groups/index.tsx
+++ src/screen/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from 'components/Header'
 import { Container } from './styles'
+import { Highlight } from 'components/Highlight'
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Grupos" subtitle="Veja os grupos disponíveis" />
     </Container>
   )
 }
