aa9370a3bd96a4d503fc810fa22abad20b3df24e
paiva <lcspaiva87@gmail.com>
2024-05-18 22:37:42 -0300

    feat: update dependencies and add @react-navigation/native

--- src/screen/NewGroup/index.tsx
+++ src/screen/NewGroup/index.tsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { Alert } from 'react-native'
 
-import { AppError } from '@utils/AppError'
-import { groupCreate } from 'storage/group/groupCreate'
+import { AppError } from 'utils/AppError'
 
 import { Container, Content, Icon } from './styles'
 
@@ -11,12 +10,11 @@ import { Header } from 'components/Header'
 import { Button } from 'components/Button'
 import { Highlight } from 'components/Highlight'
 import { Input } from 'components/Input'
+import { groupCreate } from 'storage/group/groupCreate'
 
 export function NewGroup() {
   const [group, setGroup] = useState('')
-
   const navigation = useNavigation()
-
   async function handleNew() {
     try {
       if (group.trim().length === 0) {
@@ -38,15 +36,12 @@ export function NewGroup() {
   return (
     <Container>
       <Header showBackButton />
-
       <Content>
         <Icon />
-
         <Highlight
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-
         <Input placeholder="Nome da turma" onChangeText={setGroup} />
 
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
