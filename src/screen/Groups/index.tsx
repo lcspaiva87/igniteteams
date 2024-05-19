41bd91bd551c8b8b08b2cbfd59ecc752460f4a6a
paiva <lcspaiva87@gmail.com>
2024-05-18 21:52:26 -0300

    feat: add GroupCard and ListEmpty components

--- src/screen/Groups/index.tsx
+++ src/screen/Groups/index.tsx
@@ -1,12 +1,29 @@
 import { Header } from 'components/Header'
 import { Container } from './styles'
 import { Highlight } from 'components/Highlight'
+import { GroupCard } from 'components/GroupCard'
+import { useEffect, useState } from 'react'
+import { FlatList } from 'react-native'
+import { ListEmpty } from 'components/ListEmpty'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+  useEffect(() => {
+    setGroups(['Turma A', 'Turma B', 'Turma C'])
+  }, [])
   return (
     <Container>
       <Header />
       <Highlight title="Grupos" subtitle="Veja os grupos disponíveis" />
+      <FlatList
+        data={groups}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        keyExtractor={(item, index) => String(index)}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </Container>
   )
 }
--- src/screen/Groups/styles.ts
+++ src/screen/Groups/styles.ts
@@ -1,40 +1,8 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
-  align-items: center;
-  display: flex;
-  background-color: ${(props) => props.theme.COLORS.GRAY_600};
-`
-
-export const Title = styled.Text`
-  font-size: 24px;
-  color: ${(props) => props.theme.COLORS.WHITE};
-  font-weight: bold;
-  margin-top: 10px;
-`
-export const SubTitle = styled.Text`
-  font-size: 16px;
-  color: ${(props) => props.theme.COLORS.GRAY_300};
-  font-weight: normal;
-  margin-top: 10px;
-`
-export const Button = styled.TouchableOpacity`
-  background-color: ${(props) => props.theme.COLORS.GRAY_500};
-  padding: 10px 20px;
-  border-radius: 5px;
-  margin-top: 20px;
-  border-radius: 6px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 380px;
-  height: 90px;
-  gap: 24px;
-`
-
-export const ButtonText = styled.Text`
-  color: ${(props) => props.theme.COLORS.WHITE};
-  font-size: 18px;
-  font-weight: normal;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
 `
